baa2be8bc6392244456b984d3a88b5a44289f6e0
Restyled.io <commits@restyled.io>
2024-01-13 17:51:54 +0000

    Restyled by pg_format

--- models/example/stg_customers.sql
+++ models/example/stg_customers.sql
@@ -1,10 +1,13 @@
-With stg_customers AS (
-   SELECT
-      customer_id,
-      CONCAT(first_name, ' ', last_name) as customer_name,
-      email as email_address,
-      address as billing_address
-   FROM raw.customer
+WITH stg_customers AS (
+    SELECT
+        customer_id,
+        CONCAT(first_name, ' ', last_name) AS customer_name,
+        email AS email_address,
+        address AS billing_address
+    FROM
+        raw.customer
 )
-
-SELECT * from stg_customers
+SELECT
+    *
+FROM
+    stg_customers
